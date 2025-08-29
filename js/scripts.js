a8ebc6d56fd178bccdb550ebd7cd7b861e7ef8bf
granty <granteverist@gmail.com>
2025-08-29 19:31:25 -0400

    updated lots on all pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 // not sure if i'll put js here yet
+
+function toggleContent(contentId, arrowId) {
+    const content = document.getElementById(contentId);
+    const arrow = document.getElementById(arrowId);
+
+    content.classList.toggle('open');
+    arrow.classList.toggle('open');
+}
